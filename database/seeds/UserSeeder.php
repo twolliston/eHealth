1cf9e37fe7a26a71da8265a029ae53498c6e48c9
chefgonzo <chefgonzo@gmail.com>
2018-09-21 12:12:15 -0400

    added admin, auth, and created database/updated .env

---
+++ database/seeds/UserSeeder.php
@@ -0,0 +1,19 @@
+<?php
+
+use Illuminate\Database\Seeder;
+
+use App\User;
+
+class UserSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        //
+        factory(user::class, 50)->create();
+    }
+}
